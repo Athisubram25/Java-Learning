af0d25837c82fd1ddbc5f74c00b06db0ba75f705
Athisubram <athisubram@archimedis.io>
2025-05-26 11:50:38 +0530

    Task COmpleted

--- Calculator_thread/E.java
+++ Calculator_thread/E.java
@@ -2,46 +2,30 @@ package Calculator_thread;
 
 public class E extends Thread
 {
-	public void run() 
-	{
-		System.out.println("Thread started "+ Thread.currentThread().getName());
-		System.out.println();
-		try {
-		
-		A add = new A(); 
-		add.run();
-		add.join();
-		Thread.sleep(2000);
-		
-		B div = new B();
-		div.run();
-		div.join();
-		Thread.sleep(2000);
-		
-		C multiply = new C();
-		multiply.run();
-		multiply.join();
-		Thread.sleep(2000);
-		
-		D sub = new D();
-		sub.run();
-		sub.join();
-		Thread.sleep(2000);
-		} 
-		catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
 	public static void main(String[] args) throws InterruptedException 
 	{
-		E thread = new E();
-		thread.start();
-		thread.join();
-		System.out.println();
-		System.out.println("Thread FInished");
+		A a = new A();
+		B b = new B();
+		C c = new C();
+		D d = new D();
 		
+		a.start();
+		a.join();
+		a.sleep(2000);
+		
+		b.start();
+		b.join();
+		b.sleep(2000);
+		
+		c.start();
+		c.join();
+		c.sleep(2000);
+		
+		d.start();
+		d.join();
+		d.sleep(2000);
+		
+		System.out.println("Thread Finished");
 
 	}
 }
